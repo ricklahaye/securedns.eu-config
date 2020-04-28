41116c943220860b02380b2eb9d906625161791b
Rick Lahaye <ricklahaye@outlook.com>
2020-04-28 19:46:00 +0200

    fix wrong exit code
    bash syntax is weird..

--- haproxy/haproxy_check_doh.sh
+++ haproxy/haproxy_check_doh.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 real_ip=$3
-doh=$(/bin/nc -vz $real_ip 8053; echo $?)
-named=$(/bin/nc -vz $real_ip 53; echo $?)
-if [ $doh == "0" ] && [ $named == "0" ]; then
+/bin/nc -vz $real_ip 53
+named=$?
+
+/bin/nc -vz $real_ip 8053
+doh=$?
+
+echo $named
+echo $doh
+if [[ $named == 0 ]] && [[ $named == 0 ]]; then
   exit 0
 else
   exit 1
